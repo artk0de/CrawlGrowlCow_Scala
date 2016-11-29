888ce153499eb372d37226ca892c1d072a30cd9c
art2rik <art2rik.desperado@gmail.com>
2016-11-29 08:13:48 +0300

    CrawlDaemon, CrawlRequest, CrawlResponse, CrawlResult ready

--- src/scala/crawlgrowlcow/TaskManager.scala
+++
@@ -1,8 +0,0 @@
-package crawlgrowlcow
-
-/**
-  * Created by art2rik1 on 21.11.16.
-  */
-class TaskManager {
-
-}
--- src/scala/crawlgrowlcow/crawling/CrawlResult.scala
+++
@@ -1,8 +0,0 @@
-package scala.crawlgrowlcow.crawling
-
-/**
-  * Created by art2rik1 on 28.11.16.
-  */
-class CrawlResult {
-
-}
